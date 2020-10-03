c4ae89d0f15cd2d81035f12ed172933f4324564a
JagratPatkar <jagratpatkar@gmail.com>
2020-10-04 00:03:34 +0530

    Update

--- JackAnalyzer/JackAnalyzer.py
+++ JackAnalyzer/JackAnalyzer.py
@@ -1,14 +1,41 @@
+import glob
+import sys
+
 class Tokenizer():
-    pass
+
+    def __init__(self,file):
+        pass
+    
+    def tokenize(self):
+        pass
 
 class Compiler():
     pass
 
 class Analyzer():
-    pass
+    
+    def __init__(self,path):
+        self.path = path
+        self.files = []
+        self.determine()
+
+    def determine(self):
+        dest = self.path.split("/").pop()
+        if ".jack" in dest : self.files.append(self.path)
+        else: [self.files.append(fn) for fn in glob.iglob(self.path+"/*.jack")]
+        
+    def analyze(self):
+        for fp in self.files:
+            tokenizer = Tokenizer(fp)
+            tokenizer.tokenize()
+            # compiler = Compiler(fn,tokenizer)
+        
+
 
 def main():
-    print("Welcome to Jack Analyzer")
+    path = sys.argv[1]
+    analyzer = Analyzer(path)
+    analyzer.analyze()  
 
 if __name__ == "__main__":
     main()
