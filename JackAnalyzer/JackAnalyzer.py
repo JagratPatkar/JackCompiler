233c97f01d90b33982245dcbf8ab9bb6d9bc6b2e
JagratPatkar <jagratpatkar@gmail.com>
2020-10-03 23:13:17 +0530

    Update

--- JackAnalyzer/JackAnalyzer.py
+++ JackAnalyzer/JackAnalyzer.py
@@ -1,1 +1,14 @@
-print("Welcome to JackAnalyzer")
+class Tokenizer():
+    pass
+
+class Compiler():
+    pass
+
+class Analyzer():
+    pass
+
+def main():
+    print("Welcome to Jack Analyzer")
+
+if __name__ == "__main__":
+    main()
